cf684b0d4541ba67f209952917647eaa229f7ae8
Jan Catrysse <23419642+jcatrysse@users.noreply.github.com>
2025-06-21 13:47:18 +0200

    Improve dependency behavior

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,3 +11,10 @@
   * List fields may depend on regular lists or other dependable lists.
   * Key/value fields may depend on enumerations or other dependable key/value lists.
 
+## 0.0.3
+
+* Added dependency matrix to configure allowed parent/child values.
+* Child fields are disabled until a parent value is chosen and only allowed
+  values are shown. Blank options remain available for deselection and all
+  descendant fields update when parents are cleared.
+
--- lib/redmine_depending_custom_fields/dependable_list_format.rb
+++ lib/redmine_depending_custom_fields/dependable_list_format.rb
@@ -2,7 +2,7 @@ module RedmineDependingCustomFields
   class DependableListFormat < Redmine::FieldFormat::ListFormat
     add 'dependable_list'
     self.form_partial = 'custom_fields/formats/dependable_list'
-    field_attributes :parent_custom_field_id
+    field_attributes :parent_custom_field_id, :value_dependencies
 
     def label
       :label_dependable_list
@@ -17,12 +17,33 @@ module RedmineDependingCustomFields
         custom_field.parent_custom_field_id = parent&.id
       end
     end
+
+    def possible_values_options(custom_field, object = nil)
+      options = super
+      options.unshift(['', '']) unless options.any? { |o| (o.is_a?(Array) ? o[1] : o).to_s.blank? }
+
+      if object && custom_field.parent_custom_field_id.present?
+        parent = CustomField.find_by(id: custom_field.parent_custom_field_id)
+        if parent
+          parent_value = object.custom_field_value(parent)
+          mapping = custom_field.value_dependencies || {}
+          allowed = Array(mapping[parent_value.to_s])
+          if allowed.any?
+            options = options.select do |o|
+              val = o.is_a?(Array) ? o[1] : o
+              val.to_s.blank? || allowed.include?(val.to_s)
+            end
+          end
+        end
+      end
+      options
+    end
   end
 
   class DependableEnumerationFormat < Redmine::FieldFormat::EnumerationFormat
     add 'dependable_enumeration'
     self.form_partial = 'custom_fields/formats/dependable_enumeration'
-    field_attributes :parent_custom_field_id
+    field_attributes :parent_custom_field_id, :value_dependencies
 
     def label
       :label_dependable_enumeration
@@ -36,6 +57,27 @@ module RedmineDependingCustomFields
                                      field_format: ['enumeration', RedmineDependingCustomFields::FIELD_FORMAT_DEPENDABLE_ENUMERATION])
         custom_field.parent_custom_field_id = parent&.id
       end
+    end
+
+    def possible_values_options(custom_field, object = nil)
+      options = super
+      options.unshift(['', '']) unless options.any? { |o| (o.is_a?(Array) ? o[1] : o).to_s.blank? }
+
+      if object && custom_field.parent_custom_field_id.present?
+        parent = CustomField.find_by(id: custom_field.parent_custom_field_id)
+        if parent
+          parent_value = object.custom_field_value(parent)
+          mapping = custom_field.value_dependencies || {}
+          allowed = Array(mapping[parent_value.to_s])
+          if allowed.any?
+            options = options.select do |o|
+              val = o.is_a?(Array) ? o[1] : o
+              val.to_s.blank? || allowed.include?(val.to_s)
+            end
+          end
+        end
+      end
+      options
     end
   end
 end
